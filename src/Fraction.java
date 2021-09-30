7d9ddc4df637e3c517653dcf4241bcd8ccb9ba7f
uvsq22006817 <naimabouchrit15@gmail.com>
2021-09-30 15:22:00 +0200

    midifier fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,19 @@
 public class Fraction {
+    private int numerateur;
+    private int denominateur;
+
+
+    Fraction(int num,int denom)
+    {
+        this.numerateur = num;
+        this.denominateur = denom;
+    }
+
+    public String toString()
+    {
+        String str = this.numerateur + "et " + this.denominateur;
+        return str;
+    }
+
+
 }
